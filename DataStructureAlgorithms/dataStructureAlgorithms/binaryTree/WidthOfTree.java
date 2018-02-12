e4326fe75af84e1d3e259872d62346d0b8872111
Akanksha Rajhans <RajhanAk@HYRD5CG73013ZG.ES.AD.ADP.com>
2018-02-13 01:56:05 +0530

    Testing commit

--- DataStructureAlgorithms/dataStructureAlgorithms/binaryTree/WidthOfTree.java
+++ DataStructureAlgorithms/dataStructureAlgorithms/binaryTree/WidthOfTree.java
@@ -44,7 +44,7 @@ public class WidthOfTree extends BinaryTree{
         tree.root.left.right = new Node(5);
         tree.root.right.left = new Node(6);
         tree.root.right.right = new Node(7);
-        System.out.println(tree.findWidth(tree.root));
+        System.out.println("Width of tree is: "+tree.findWidth(tree.root));
         tree.findWidth(tree.root);
 	}
 
